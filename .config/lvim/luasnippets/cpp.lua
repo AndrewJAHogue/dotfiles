346c20380089bc84b885b4a289e9d4402e2728e3
AndrewJAHogue <andrewjahogue@gmail.com>
2022-12-20 16:54:01 -0600

    Add .config/lvim

---
+++ .config/lvim/luasnippets/cpp.lua
@@ -0,0 +1,15 @@
+return {
+  s("_cout", {t "wha t the hell"}),
+  s("BEGIN", {t {
+			"#include <iostream>",
+			"#include <string>",
+			"",
+			"using namespace std;",
+			"",
+			"int main(){",
+
+        "return 0;",
+      "}"
+      }
+}),
+}
